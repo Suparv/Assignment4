82813ad6df45874d1d123715c3c6f6357135f296
Suparv <60036042+Suparv@users.noreply.github.com>
2022-11-05 23:35:13 +0530

    Add files via upload

---
+++ Ques1.java
@@ -0,0 +1,32 @@
+SID - 19105022
+package Assignment4;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of vector you want to create");
+        int n = sc.nextInt();
+        Vector<String> userVector = new Vector<>(n);
+        int i = 0;
+        while(i<n)
+        {
+            String s = sc.next();
+            if(s.equals("terminating")){
+                break;
+            }
+            else{
+                i++;
+                userVector.addElement(s);
+            }
+        }
+        for(int j = 0;j< userVector.size();j++){
+            System.out.print(userVector.elementAt(j)+" ");
+        }
+        System.out.println();
+        System.out.println("Vector size is : " + userVector.size() + " and Vector capacity is : " + userVector.capacity());
+
+    }
+}
